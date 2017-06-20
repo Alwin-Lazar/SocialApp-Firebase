43de23d31463443eb3235d558cc16d1d706f1272
Alwin Lazar V <alv@Apples-MacBook-Pro.local>
2017-06-20 12:34:25 +0530

    Complete Firebase storage download

--- SocialApp-firebase/PostCell.swift
+++ SocialApp-firebase/PostCell.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
-
+    
     @IBOutlet weak var profileImg: CircleImageView!
     @IBOutlet weak var usernameLbl: UILabel!
     @IBOutlet weak var postImg: UIImageView!
@@ -23,9 +24,56 @@ class PostCell: UITableViewCell {
         
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.captionTextView.text = post.caption
         self.likesLbl.text = "\(post.likes)"
+        
+        if img != nil {
+            self.postImg.image = img
+        } else {
+            
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error) in
+            
+                if error != nil {
+                    print("ALV: unable to download image from firebase storage")
+                } else {
+                    print("ALV: Image downloaded from Firebase storage")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
